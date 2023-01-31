824175a199c907f59dd26998f6f33a8bb3606b4d
lukas <lordbecvold@gmail.com>
2023-01-31 16:29:22 +0100

    [+] Navigation panel

--- site/components/MainComponent.php
+++ site/components/MainComponent.php
@@ -2,5 +2,8 @@
     <?php 
         // import main user bar
         include_once("elements/UserBar.php");
+
+        // import navigation panel
+        include_once("elements/Navigation.php");
     ?>
 </main>
---
+++ site/components/elements/Navigation.php
@@ -0,0 +1,9 @@
+<div class="navigation-bar">
+    <span class="left">
+        <a class="nav-link" href="/"><i class="fas fa-home"></i></a>
+    </span>
+    <form class="right nav-search-form" method="post" action="#">
+        <input class="user-bar-link" type="text" name="search-value" placeholder="Search">
+        <button class="user-bar-link" type="submit" name="search-submit"><i class="fas fa-search"></i></button>
+    </form>
+</div>
