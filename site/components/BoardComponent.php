0bf7c57dc4a601938fa3415495f5925e80a95499
lukas <lordbecvold@gmail.com>
2023-02-02 15:20:45 +0100

    [+] main forum selector

--- site/components/BoardComponent.php
+++ site/components/BoardComponent.php
@@ -1,4 +1,137 @@
-<?php 
+<div class="forum-board">
+    <p class="forum-title">FORUM</p>
+    <div>
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 1</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
 
-    echo "forum board";
-?>
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 2</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 3</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 4</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 5</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+    </div>
+</div>
+
+<div class="forum-board">
+    <p class="forum-title">FORUM 1</p>
+    <div>
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 1</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 2</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 3</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 4</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 5</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+    </div>
+</div>
+
+<div class="forum-board">
+    <p class="forum-title">FORUM 2</p>
+    <div>
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 1</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 2</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 3</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 4</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+        <p class="forum-category">
+            <div class="forum-line">
+                <a class="forum-link" href="#">CATEGORY 5</a>
+                <span class="right posts-count">posts: 35</span>
+            </div>
+            <p class="category-desc"><i class="fas fa-dot-circle"></i> category 1 description bla blab bla category 1 description bla blab bla</p>
+        </p>
+
+    </div>
+</div>
