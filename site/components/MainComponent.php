261e1ba1de7c092ed92a4bec5d7f5b848c44ebb6
lukas <lordbecvold@gmail.com>
2023-02-02 15:39:02 +0100

    [+] footer

--- site/components/MainComponent.php
+++ site/components/MainComponent.php
@@ -8,5 +8,8 @@
 
         // import forum board
         include_once("BoardComponent.php");
+
+        // import page footer
+        include_once("elements/Footer.php");
     ?>
 </main>
---
+++ site/components/elements/Footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <p class="footer-text">
+        <?php 
+            echo date("Y") . " © <a class='author-link' href='".$pageConfig->getValueByName("authorLink")."' target='_blank'>".$pageConfig->getValueByName("author")."</a>";
+        ?>
+    </p>
+</div>
