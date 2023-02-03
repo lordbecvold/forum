fa5b249e383c88e19c5d436a5731569ae696701a
lukas <lordbecvold@gmail.com>
2023-02-03 14:57:03 +0100

    [+] my posts link (logged users only)

--- site/components/elements/ForumBorwserSubPanel.php
+++ site/components/elements/ForumBorwserSubPanel.php
@@ -1,6 +1,15 @@
 <div class="sub-panel">
     <p>
         <a class="basic-link" href="#">NEW POST</a>
-        <a class="basic-link" href="#">MY POSTS</a>
+        <?php // and my posts link to sub panel (if user logged in)
+            if ($userController->isUserLogged()) {
+                echo '<a class="basic-link" href="#">MY POSTS</a>';
+            }
+        ?>
+        <span class="right sort-panel">
+            <span class="sort-text">sort by: </span>
+            <a class="basic-link" href="#">CREATED</a>
+            <a class="basic-link" href="#">UPDATE</a>
+        </span>
     </p>
 </div>
