0f3907800e7ddf3a85912efe22ec792d0b75afdf
lukas <lordbecvold@gmail.com>
2023-02-07 15:55:25 +0100

    user data get fix

--- framework/app/controller/controllers/UserController.php
+++ framework/app/controller/controllers/UserController.php
@@ -141,18 +141,13 @@
 
 			global $mysqlUtils;
 
-			// check if user token is not null
-			if ($this->getUserToken() != NULL) {
-
-				// return user profile pic
-				return $mysqlUtils->readFromMysql("SELECT image_base64 FROM users WHERE username = '".$username."'", "image_base64");
-			} else {
-				$this->logout();
-			}
+			// return user profile pic
+			return $mysqlUtils->readFromMysql("SELECT image_base64 FROM users WHERE username = '".$username."'", "image_base64");
 		}
 
 		// get user role by name
 		public function getUserRoleByName($username) {
+			
 			global $mysqlUtils;
 			global $pageConfig;
 
@@ -162,31 +157,25 @@
 			// check if token exist in users
 			if ($count == "1") {
 
-				// check if user token is null
-				if ($this->getUserToken() != NULL) {
-
-					// get user role from database
-					$userRole = $mysqlUtils->readFromMysql("SELECT role FROM users WHERE username = '".$username."'", "role");
+				// get user role from database
+				$userRole = $mysqlUtils->readFromMysql("SELECT role FROM users WHERE username = '".$username."'", "role");
 					 
-					// get user role output
-					if (strtolower(($userRole)) == "owner") {
-						$role = "Owner";
-					} elseif (strtolower($userRole) == "admin") {
-						$role = "Admin";
-					} elseif (strtolower($userRole) == "developer") {
-						$role = "Developer";
-					} elseif (strtolower($userRole) == "vip") {
-						$role = "VIP";
-					} else {
-						$role = "User";
-					}
-				
-					// final role output
-					return $role;
-					
+				// get user role output
+				if (strtolower(($userRole)) == "owner") {
+					$role = "Owner";
+				} elseif (strtolower($userRole) == "admin") {
+					$role = "Admin";
+				} elseif (strtolower($userRole) == "developer") {
+					$role = "Developer";
+				} elseif (strtolower($userRole) == "vip") {
+					$role = "VIP";
 				} else {
-					return NULL;
+					$role = "User";
 				}
+				
+				// final role output
+				return $role;
+
 			} else {
 				return NULL;
 			}
