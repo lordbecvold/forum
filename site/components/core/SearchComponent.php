b3f4cc5ad6d4026569d6d6bfae3464ed9d82ab63
lukas <lordbecvold@gmail.com>
2023-02-08 14:20:49 +0100

    empty search check

--- site/components/core/SearchComponent.php
+++ site/components/core/SearchComponent.php
@@ -3,7 +3,13 @@
     // check if used search form
     if (isset($_POST["search-submit"])) {
 
-        echo $_POST["search-value"];
+        $search = $_POST["search-value"];
+        if (empty($search)) {
+            echo "search si empty";
+        } else {
+
+            echo "search: " . $search;
+        }
     }
 
 ?>
