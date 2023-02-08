672a816501407fdb114a6a6b816c4a62b5f13e66
lukas <lordbecvold@gmail.com>
2023-02-08 15:34:35 +0100

    new post title margin

--- site/components/forms/NewPostForm.php
+++ site/components/forms/NewPostForm.php
@@ -8,7 +8,7 @@
         <form class="form" action="?process=new&forum='.$forum.'" method="post">
             <p class="form-title margin-bot-8">Create new post</p>
             <input class="user-input-field new-post-title" type="text" name="name" placeholder="Title"><br>
-            <textarea class="user-input-field new-post-content" name="post-content" placeholder="Post text"></textarea><br>
+            <textarea class="user-input-field new-post-content margin-bot-8" name="post-content" placeholder="Post text"></textarea><br>
             <center>
                 <input class="user-input-field" type="submit" name="new-post-submit" value="Submit">
             </center>
