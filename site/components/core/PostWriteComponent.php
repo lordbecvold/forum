b5755a074ac6f6f78f68cbc2ea2265909c438a7a
lukas <lordbecvold@gmail.com>
2023-02-08 14:26:53 +0100

    [+] logged in check (new post create)

--- site/components/core/PostWriteComponent.php
+++ site/components/core/PostWriteComponent.php
@@ -1,10 +1,23 @@
 <?php 
 
-    // get forum name from url
-    $forum = $siteController->getQueryString("forum");  
+    // check if user logged in
+    if ($userController->isUserLogged()) {
 
-    if ($forum != null) {
-
-        echo "new post componnt in ".$forum;
+        // get forum name from url
+        $forum = $siteController->getQueryString("forum");  
+    
+        // check if forum is not null
+        if ($forum != null) {
+    
+            echo "new post componnt in ".$forum;
+        }
+    } else {
+        $alertController->normalAlert(
+            "Only a logged in user can create a new thread!
+            <br>
+            <a href='?process=login' class='basic-link'>login here</a>
+            "
+        );
     }
+
 ?>
